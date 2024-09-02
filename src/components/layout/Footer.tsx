81cfe9619b1425d3efda5d1f2ce7c89b93c2a150
warnoffline <hametshin543@gmail.com>
2024-09-02 22:38:15 +0300

    update links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,12 @@
 import plane from '@/assets/images/plane-2.png';
+import pilot from '@/assets/images/pilot.png'
 export function Footer() {
   return (
     <footer className="overflow-hidden bg-gradient-to-b from-[#000000] to-[#272525]">
       <div className="container relative w-full h-[200px] ">
+        <div className='absolute left-0 top-1/3 w-[250px]'>
+          <img src={pilot} alt="" />
+        </div>
         <div className="absolute right-0 top-0 w-[250px]">
           <img src={plane} alt="" />
         </div>
--- src/components/layout/Links.tsx
+++ src/components/layout/Links.tsx
@@ -1,35 +1,46 @@
-import rustore from '@/assets/images/rustore-logo.png';
+
 import { Button } from '../ui/button';
 import { BookDown, Globe } from 'lucide-react';
-
+import phone from '@/assets/phone-showcase/Phone.png';
+import rustore from '@/assets/images/rustore.svg'
 export function Links() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center container">
-      <h3 className="text-3xl ">Посетите.</h3>
-      <h3 className="text-3xl">Скачайте.</h3>
-      <h3 className="text-3xl bg-clip-text bg-gradient-to-r from-[#f6d365] to-[#fda085] text-transparent">
-        Установите.
-      </h3>
-      <p className="text-center text-lg">
+    <div className="w-full h-full flex flex-row justify-around">
+      <div className='w-[40%] flex flex-col gap-10 justify-center'>
+      <div className='flex flex-col gap-5'>
+        <h3 className="text-5xl font-bold">Посетите.</h3>
+        <h3 className="text-5xl font-bold">Скачайте.</h3>
+        <h3 className="text-5xl font-bold bg-clip-text bg-gradient-to-r from-[#f6d365] to-[#fda085] text-transparent">
+          Установите.
+        </h3>
+      </div>
+      <p className="text-lg">
         Выберите любой удобный вариант - нашим приложением можно пользоваться из
         браузера, установить как PWA, скачать APK или воспользоваться RuStore.
       </p>
-      <div className="flex flex-col gap-3">
-        <div className="cursor-pointer">
-          <img src={rustore} alt="Rustore logo" />
-        </div>
-        <Button className="flex gap-2 text-xl bg-black py-6 group">
+      <div className="w-[50%] flex flex-col gap-3">
+      <Button className="flex gap-2 text-xl bg-black py-7 group relative">
+          <div className='w-[2rem]'>
+          <img className='w-[100%] h-[100%]' src={rustore} alt="Rustore logo" />
+          </div>
+          <span className='bg-clip-text text-white group-hover:bg-gradient-to-r from-[#8454C8] via-[#C850C0] to-[#FFCC70] group-hover:text-transparent transition-all duration-300'>Скачать из RuStore</span>
+          </Button>
+        <Button className="flex gap-2 text-xl bg-black py-7 group">
           <BookDown />
           <span className="bg-clip-text text-white group-hover:bg-gradient-to-r from-[#8454C8] via-[#C850C0] to-[#FFCC70] group-hover:text-transparent transition-all duration-300">
             Скачать APK
           </span>
         </Button>
-        <Button className="flex gap-2 text-xl bg-black py-6 group">
+        <Button className="flex gap-2 text-xl bg-black py-7 group">
           <Globe />
           <span className="bg-clip-text text-white group-hover:bg-gradient-to-r from-[#8454C8] via-[#C850C0] to-[#FFCC70] group-hover:text-transparent transition-all duration-300">
             Перейти на вебсайт
           </span>
         </Button>
+      </div>
+      </div>
+      <div className='w-[30%] flex justify-center items-center'>
+      <img src={phone} alt="" />
       </div>
     </div>
   );
